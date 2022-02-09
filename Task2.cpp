8ae724cd90e4dce6af65f45a1d59be8f299ffb0f
rJAYESH <99338712+rJAYESH@users.noreply.github.com>
2022-02-09 19:41:29 +0530

    Created MyCaptain Task 2
    This file for swapping two numbers for task 2

---
+++ Task2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("Enter first number= ");
+	scanf("%d",&num1);
+	printf("Enter the second number= ");
+	scanf("%d",&num2);
+	printf("Swapping.....");
+	printf("After swap first number=%d second number=%d",num2,num1);
+	return 0;
+}
